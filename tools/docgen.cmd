9b4ed27809444a7b76d845957528015430d3ca12
Aptivi <eoflaoevicecity@gmail.com>
2024-03-10 12:51:18 +0300

    imp - Updated docfx build script to use latest version
    ---
    
    Instead of using Chocolatey, we need to use .NET tool instead.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- tools/docgen.cmd
+++ tools/docgen.cmd
@@ -1,18 +1,16 @@
 @echo off
 
-REM Copyright Drew Noakes. Licensed under the Apache-2.0 license. See the LICENSE file for more details.
-REM Copyright 2023-2024 - Aptivi. Licensed under the Apache-2.0 license. See the LICENSE file for more details.
-
-REM This script builds documentation and packs the artifacts.
+REM This script builds the documentation and packs the artifacts. Use when you have VS installed.
+for /f "tokens=* USEBACKQ" %%f in (`type version`) do set ksversion=%%f
 
 echo Finding DocFX...
-if exist %ProgramData%\chocolatey\bin\docfx.exe goto :build
-echo You don't have DocFX installed. Download and install Chocolatey and DocFX.
+if exist %USERPROFILE%\.dotnet\tools\docfx.exe goto :build
+echo You don't have DocFX installed. Download and install .NET and DocFX.
 goto :finished
 
 :build
-echo Building Documentation...
-%ProgramData%\chocolatey\bin\docfx.exe "..\DocGen\docfx.json"
+echo Building the documentation...
+%USERPROFILE%\.dotnet\tools\docfx.exe "..\DocGen\docfx.json"
 if %errorlevel% == 0 goto :success
 echo There was an error trying to build documentation (%errorlevel%).
 goto :finished
